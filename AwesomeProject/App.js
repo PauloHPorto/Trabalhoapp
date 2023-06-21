61a1e168fc2a9d4fde784a92bf37aa2bc7989191
Paulo Henrique Porto <127361154+PauloHPorto@users.noreply.github.com>
2023-06-21 19:50:37 -0300

    aaa

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>vamooooioi</Text>
+      <Text>hgjhgkjhgjk</Text>
       <StatusBar style="auto" />
     </View>
   );
