411b9e9efeba6b449b88fe7890b087017e18021d
Paulo Henrique Porto <127361154+PauloHPorto@users.noreply.github.com>
2023-06-21 19:34:59 -0300

    Tentativa e erro

---
+++ src/pages/login/inde.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+function Login() {
+  return (
+    <View>
+      <Text>Bem vindo a tela de login</Text>
+    </View>
+  );
+}
+
+export default Login;
